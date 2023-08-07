29c41c604a28459d82de400308cb0282b5edb964
jurichar <jurichar@student.42.fr>
2023-08-07 18:15:46 +0200

    add deploy and service

--- p3/deploy.sh
+++ p3/deploy.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
+#!bin/bash
 
+sudo k3d cluster create part3
+
+sudo kubectl config use-context k3d-part3
+
+sudo kubectl create namespace dev
+sudo kubectl create namespace argocd
+
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+sudo kubectl apply -f App/app-deploy.yaml -n dev
+sudo kubectl apply -f App/app-service.yaml -n dev
+
+sudo kubectl get svc -n dev
+sudo kubeclt get deployments -n dev
