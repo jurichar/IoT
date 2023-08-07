aa8b445ca1dca42a9004b3a583761275107d7df8
macrespo42 <macrespo@student.42.fr>
2023-08-07 15:04:37 +0200

    feat: install kubectl

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+function install_docker() {
+	for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg -y
+	sudo apt-get update -y
+	sudo apt-get install ca-certificates curl gnupg -y
 
-sudo install -m 0755 -d /etc/apt/keyrings -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+	sudo install -m 0755 -d /etc/apt/keyrings -y
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	echo \
+  	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update -y
+	sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+	echo "docker installed!"
+}
+
+function install_kubectl() {
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+	echo "kubectl installed!"
+	rm kubectl
+}
+
+function install_dependencies() {
+	install_docker
+	install_kubectl
+}
+
+install_dependencies
