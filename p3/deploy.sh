7ca34783adaf5a3b162a3e524956193f51647886
jurichar <jurichar@student.42.fr>
2023-08-08 17:29:48 +0200

    add application yaml

--- p3/deploy.sh
+++ p3/deploy.sh
@@ -15,6 +15,8 @@ sudo kubectl apply -f App/app-service.yaml -n dev
 password=$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 echo "admin" > id_agrocd.txt
+echo "password :"
+echo $password 
 echo $password >> id_agrocd.txt
 
 while [[ $(sudo kubectl get pods -n argocd -o 'jsonpath={..status.containerStatuses[*].ready}') != "true true true true true true false" || $(sudo kubectl get pods -n dev -o 'jsonpath={..status.containerStatuses[*].ready}') != "true" ]]; do
@@ -25,6 +27,8 @@ done
 sudo kubectl get services -n argocd
 sudo kubectl get services -n dev
 sudo kubectl get deployments -n dev
+
+sudo kubectl apply -f mpochard-config/application.yaml 
 
 # sleep 30
 
