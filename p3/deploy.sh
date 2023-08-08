d6b46ea0a142c52d807fbc6b4bb93ccacee6ca94
jurichar <jurichar@student.42.fr>
2023-08-08 13:03:13 +0200

    add password argo

--- p3/deploy.sh
+++ p3/deploy.sh
@@ -26,6 +26,11 @@ sudo kubectl config use-context k3d-part3
 deploy_dev
 deploy_argocd
 
+password=$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+echo "admin" > id_agrocd.txt
+echo $password >> id_agrocd.txt
+
 sudo kubectl get svc -n dev
 sudo kubectl get deployments -n dev
 
