42d041fe8ce2db8d5b75d093f1f75eb529270471
jurichar <jurichar@student.42.fr>
2023-08-11 16:41:18 +0200

    deploy

--- p3/deploy.sh
+++ p3/deploy.sh
@@ -20,9 +20,9 @@ else
     echo -e "${RED}Error applying ArgoCD.${NC}"
 fi
 
-
 while [[ $(sudo kubectl get pods -n argocd -o 'jsonpath={..status.containerStatuses[*].ready}' 2>/dev/null) != "true true true true true true true" ]]; do    
     echo -e "${YELLOW}Waiting for pods agrocd...${NC}"
+    sudo kubectl get pods -n argocd
     sleep 20
 done
 
@@ -45,6 +45,7 @@ fi
 
 while [[ $(sudo kubectl get pods -n dev -o 'jsonpath={..status.containerStatuses[*].ready}' 2>/dev/null) != "true" ]]; do    
     echo -e "${YELLOW}Waiting for pods dev...${NC}"
+    sudo kubectl get pods -n dev
     sleep 20
 done
 
@@ -57,6 +58,7 @@ while 1; do
     if [[ $(sudo kubectl get pod -n dev -o 'jsonpath={..status.containerStatuses[*].ready}' 2>/dev/null) != "true" ]]; then
         while [[ $(sudo kubectl get pods -n dev -o 'jsonpath={..status.containerStatuses[*].ready}' 2>/dev/null) != "true" ]]; do    
             echo -e "${YELLOW}Waiting for pods dev...${NC}"
+            sudo kubectl get pods -n dev
             sleep 20
         done
         sudo kubectl port-forward -n dev service/wil-service 8888:8888 &
