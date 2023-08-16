1d5409a30f13f23b2b1e6dfb45bd7c8c22feb91d
macrespo42 <macrespo@student.42.fr>
2023-08-16 16:34:56 +0200

    fix: script part2

--- p2/start.sh
+++ p2/start.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Start vagrant
 vagrant up
 
+sudo cp /etc/host /etc/host.backup
+
 # Add hosts
 sudo echo "192.168.56.110  app1.com" >> /etc/hosts
 sudo echo "192.168.56.110  app2.com" >> /etc/hosts
-
-# Wait kubectl in vagrant up
-sleep 30
-
-# Open app in browser
-firefox app1.com &
-firefox app2.com &
